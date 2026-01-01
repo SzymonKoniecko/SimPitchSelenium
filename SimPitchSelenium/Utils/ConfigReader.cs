5027065be2c23ad1976e5003e9f9e97c2c36aaa6
Szymon Koniecko <s-koniecko@wp.pl>
2026-01-02 00:56:53 +0100

    added new fields to reader

--- SimPitchSelenium/Utils/ConfigReader.cs
+++ SimPitchSelenium/Utils/ConfigReader.cs
@@ -2,14 +2,21 @@ using System;
 using Newtonsoft.Json.Linq;
 
 namespace SimPitchSelenium.Utils;
-
 public static class ConfigReader
 {
-    private static readonly string configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+    private static readonly string configPath =
+        Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
 
     public static string GetBaseUrl() => Get("baseUrl");
     public static string GetBrowser() => Get("browser");
     public static bool GetHeadless() => bool.Parse(Get("headless"));
+
+    public static string GetDriverMode() => Get("driverMode");
+    public static string GetRemoteUrl() => Get("remoteUrl");
+
+    public static int GetImplicitTimeoutSec() => int.Parse(Get("timeouts.implicit"));
+    public static int GetExplicitTimeoutSec() => int.Parse(Get("timeouts.explicit"));
+
     public static string Get(string key)
     {
         var json = File.ReadAllText(configPath);
