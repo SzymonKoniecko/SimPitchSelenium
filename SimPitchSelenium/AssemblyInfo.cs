0cdb2635fe0a25e4bfac19e7f04fa2ae6f3afa88
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-17 01:00:45 +0200

    parallelism

---
+++ SimPitchSelenium/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+
+// Run tests in parallel across different test fixtures (classes)
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+// Set the number of parallel workers (threads). 
+// 4 is a safe default for Selenium to avoid overloading the local machine.
+// Can be adjusted or removed to default to the number of CPU cores.
+[assembly: LevelOfParallelism(4)]
