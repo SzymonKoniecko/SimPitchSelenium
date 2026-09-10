c95a57755943a0f3bdfc4b63ab83e0543aa4f8db
Szymon Koniecko <s-koniecko@wp.pl>
2026-09-10 16:29:42 +0200

    upgrade model

--- SimPitchSelenium/Models/PrepareSimulationModel.cs
+++ SimPitchSelenium/Models/PrepareSimulationModel.cs
@@ -7,7 +7,9 @@ public class PrepareSimulationModel
     public bool isSeason2022_2023 { get; set; } = false;
     public bool isSeason2023_2024 { get; set; } = false;
     public bool isSeason2024_2025 { get; set; } = false;
-    public bool isSeason2025_2026 { get; set; } = true;
+    public bool isSeason2025_2026 { get; set; } = false;
+    public bool isSeason2026_2027 { get; set; } = true;
+
 
     public string? Title { get; set; }
     public string? Model { get; set; }
